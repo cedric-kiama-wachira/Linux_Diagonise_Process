ba904c4610e34cd17afdaf3039ce9d08d33d96bf
Cedric K. Wachira <cedric.kiama@gmail.com>
2023-09-29 16:18:14 +0400

    Updated the ps.sh file

--- ps.sh
+++ ps.sh
@@ -2,4 +2,9 @@
 
 ps -a #Unix style syntax
 ps a  #BSD style syntax
+ps -aux #The U option is for user based mode
+
+kill -L # Typing this command and option plus pressing the tab key shows all options that accompany the kill process
+pgrep -a process_name # Get the process ID
+lsof -p process_id  # Get the list of files and directories being used by the process
 
